0c798fbfc14689314f9093cef7130f71a11f3319
Pathomph <t-phathomphon@hotmail.com>
2018-11-26 16:53:28 +0700

    axios

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Users from './views/Users.vue'
 import Products from './views/Products.vue'
+import Users from './views/๊Users.vue'
 
 
 Vue.use(Router)
